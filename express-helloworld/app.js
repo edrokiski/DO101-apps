23748b19e877302ad1cfd58379e2c664a17309dc
Diaz Rokiski <elena.diaz@atos.net>
2020-04-28 00:04:51 +0200

    add /elena endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/elena', function (req, res) {
+  res.send('Hello Elena!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
